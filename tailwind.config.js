3a6ebfcbf787cf9a14f7ef4b9e0e403a1ce8a4b7
Helen Tsao <helen.tsao@isobar.com>
2025-06-24 12:12:48 +0800

    第一第二天code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class',
+   content: [
+    "./src/**/*.html", // 包含所有 src 目錄下的 html
+    "./src/**/*.js",   // 如果有 js/ts 檔案也要加
+  ],
   theme: {
     extend: {},
   },
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+const htmlFiles = fs.readdirSync(path.resolve(__dirname, 'src'))
+  .filter(file => file.endsWith('.html'));
 
 module.exports = {
   entry: './src/index.js',
@@ -42,9 +46,22 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+     ...htmlFiles.map(filename => new HtmlWebpackPlugin({
+      template: `./src/${filename}`,
+      filename: filename,
+    })),
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html',
+    //   filename: 'index.html',
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/tailwind-course-1.html',
+    //   filename: 'tailwind-course-1.html',
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/tailwind-course-2.html',
+    //   filename: 'tailwind-course-2.html',
+    // }),
   ],
   devServer: {
     static: {
